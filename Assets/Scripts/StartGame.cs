ab5494cc5f158caf172c2b808de2367263731904
Maria Smirnova <92306135+MarieMittu@users.noreply.github.com>
2024-10-17 19:26:29 +0200

    added ui timer clock-like

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
@@ -11,30 +12,49 @@ public class StartGame : MonoBehaviour
     public float startNightDuration;
     float secondTimer = 0f;
 
+    public Image nightTimer;
+    private bool timerStarted = false;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        if (nightTimer != null)
+        {
+            float initialFillAmount = 0.67f; // Set your initial fill amount
+            nightTimer.fillAmount = initialFillAmount; // Visual representation
+            startNightDuration = nightDuration / initialFillAmount;
+            UnityEngine.Debug.Log("TIMERRR Initial Fill Amount: " + nightTimer.fillAmount);
+            UnityEngine.Debug.Log("TIMERRR Start Night Duration: " + startNightDuration);
+        }
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        secondTimer = secondTimer + Time.deltaTime;
-        if (secondTimer >= 1f)
+        if (timerStarted)
         {
-            nightDuration--;
-            secondTimer = secondTimer - 1f;
+            secondTimer = secondTimer + Time.deltaTime;
+            if (secondTimer >= 1f)
+            {
+                nightDuration--;
+                secondTimer = 0f;
 
-            
-        }
+                if (nightTimer != null)
+                {
+                    nightTimer.fillAmount = Mathf.Clamp(nightDuration / startNightDuration, 0f, 1f);
+                    UnityEngine.Debug.Log("TIMERRR Updated Fill Amount: " + nightTimer.fillAmount);
+                }
+            }
 
 
 
-        if (nightDuration <= 0)
-        {
+            if (nightDuration <= 0)
+            {
 
-            SceneManager.LoadScene("GameOverScene"); // change to normal
+                SceneManager.LoadScene("GameOverScene"); // change to normal
+            }
         }
     }
 
@@ -43,8 +63,11 @@ public class StartGame : MonoBehaviour
         if (other.CompareTag("TIMER"))
         {
             //start timer night
-            startNightDuration = nightDuration;
+            UnityEngine.Debug.Log("TIMERRR GAME BEGIN");
+            timerStarted = true;
             Destroy(other.gameObject);
+
+            
         }
     }
 
