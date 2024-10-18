9d9f50a10bdab3a6237846d6f0053f5dc6f68707
Maria Smirnova <92306135+MarieMittu@users.noreply.github.com>
2024-10-18 07:50:28 +0200

    hand grabs baby

--- Assets/Scripts/RuleFive.cs
+++ Assets/Scripts/RuleFive.cs
@@ -1,54 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
 
 public class RuleFive : MonoBehaviour
 {
     public float timeRemaining = 10;
     bool isGuarding = false;
     [SerializeField] GameObject baby;
+    bool startControl = false;
+
+    public Camera subCamera;
+    public Camera playerCamera;
+    public GameObject eye;
+    public GameObject witchesHand;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isGuarding && timeRemaining > 0)
+        if (startControl)
         {
-            timeRemaining -= Time.deltaTime;
-            //Debug.Log("GUARDA here" + timeRemaining);
+            if (!isGuarding && timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                //Debug.Log("GUARDA here" + timeRemaining);
+            }
+
+            if (timeRemaining < 1)
+            {
+                ShowBed();
+                Invoke("ShowHand", 2f);
+                Invoke("DestroyBaby", 4f);
+                Invoke("LoadGO", 9f);
+            }
         }
 
-        if (timeRemaining < 1)
+    }
+
+    public void ShowBed()
+    {
+        subCamera.enabled = true;
+        playerCamera.enabled = false;
+        Cursor.visible = false;
+        eye.SetActive(false);
+        //nanny.SetActive(false);
+        PlayerMovement playerMovement = FindObjectOfType<PlayerMovement>(); // Find the player movement script
+        if (playerMovement != null)
+        {
+            playerMovement.SetMovementState(false); // Disable movement
+        }
+        
+    }
+        void ShowHand()
+        {
+            witchesHand.SetActive(true);
+        }
+
+        //voids to invoke
+        void DestroyBaby()
         {
             Destroy(baby);
-            Debug.Log("GUARDA " + timeRemaining);
-            //game over scene
+            Destroy(witchesHand);
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("GUARDA"))
+        void LoadGO()
         {
-            //stop and reset countdown
-            Debug.Log("GUARDA okay");
-            isGuarding = true;
-            timeRemaining = 10;
+            SceneManager.LoadScene("GameOverScene");
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("GUARDA"))
+        void OnTriggerEnter(Collider other)
         {
-            //start countdown
-            Debug.Log("GUARDA madonnna");
-            isGuarding = false;
+            if (other.CompareTag("TIMER"))
+            {
+                startControl = true;
+            }
+            else
+                if (other.CompareTag("GUARDA"))
+            {
+                //stop and reset countdown
+                Debug.Log("GUARDA okay");
+                isGuarding = true;
+                timeRemaining = 10;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("GUARDA"))
+            {
+                //start countdown
+                Debug.Log("GUARDA madonnna");
+                isGuarding = false;
+            }
         }
     }
-}
